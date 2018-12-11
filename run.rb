c84cec08bc933193d7fde2078707d2c272dae2e9
Arkadiusz Buras <macbury@gmail.com>
2018-12-11 16:36:24 +0100

    Update run.rb

--- run.rb
+++ run.rb
@@ -13,20 +13,25 @@ Aws.config.update({
   )
 })
 
-QUERY = "SELECT AVG(height) as avg_height, country FROM #{ENV.fetch('TABLE_NAME')}.countries GROUP BY country;"
-
+# inicializacja klienta atheny dla rubiego
 @client = Aws::Athena::Client.new
 
+# pierwsze zapytanie do klastra atheny
+QUERY = "SELECT AVG(height) as avg_height, country FROM #{ENV.fetch('TABLE_NAME')}.countries GROUP BY country;"
+
+# Tworzymy nowe zapytanie co sobie lata w tle, i dostajemy informacje o id zapytania
+# Chwila na flashback z DFP
 query_resp = @client.start_query_execution(
-  query_string: QUERY,
+  query_string: QUERY, # nasze zapytanie
   query_execution_context: {
-    database: ENV.fetch('DATABASE_NAME'),
+    database: ENV.fetch('DATABASE_NAME'), # informacja o wybranej bazie na której ma zostać wykonane zapytanie
   },
   result_configuration: {
-    output_location: ENV.fetch('S3_BUCKET_OUTPUT')
+    output_location: ENV.fetch('S3_BUCKET_OUTPUT') # gdzie ma zostać zapisany wynikowy csv
   }
 )
 
+# rozpoczynamy pętle w której co sekunde sprawdzamy czy zapytanie sie nam zrobiło
 loop do
   status_resp = @client.get_query_execution(query_execution_id: query_resp.query_execution_id)
   puts "Query: #{query_resp.query_execution_id} status is: #{status_resp.query_execution.status.state}"
@@ -34,6 +39,7 @@ loop do
   sleep 1
 end
 
+# pobieramy wynik i go wyświeltamy
 result_resp = @client.get_query_results(query_execution_id: query_resp.query_execution_id)
 rows = result_resp.result_set.rows
                              .map { |row| [row.data[0].var_char_value, row.data[1].var_char_value] }
